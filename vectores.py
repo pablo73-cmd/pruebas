18ba41426971325a58fc047d339316f7eb0257b9
pablo73-cmd <82736226+pablo73-cmd@users.noreply.github.com>
2025-04-23 10:52:51 -0300

    Add files via upload
    Agrego una mejora al sistema principal

---
+++ vectores.py
@@ -0,0 +1,8 @@
+numeros = [4, 2, 9, 1, 5]
+numeros.sort()
+print(numeros)  # [1, 2, 4, 5, 9]
+
+palabras = ['perro', 'gato', 'elefante']
+palabras.sort(key=len)
+print(palabras)  # ['gato', 'perro', 'elefante']
+
